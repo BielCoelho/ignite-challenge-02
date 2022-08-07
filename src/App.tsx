32f90cff341011cf3f2087c945e8cbcfd3021a05
Gabriel Coelho <gabrielcuei98@gmail.com>
2022-08-07 04:00:45 +0100

    limpando codigo desnecessario

--- src/App.tsx
+++ src/App.tsx
@@ -13,17 +13,6 @@ interface GenreResponseProps {
   title: string;
 }
 
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
-
 export function App() {
   const [selectedGenreId, setSelectedGenreId] = useState(1);
   const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
